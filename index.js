4973d272db9acb3d7d21a0ebf2c7c6f6b48fffe6
NazarenoRios <nazarenolrios@gmail.com>
2022-09-30 23:17:12 -0300

    cors2

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 const PORT = 5432;
 
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:3000",
     methods: ['GET', 'POST', 'DELETE', 'OPTIONS'], credentials: true,
   }));
 
