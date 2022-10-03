f67a8f74951f5e69076b1c646af4e76a5afcc6fa
NazarenoRios <nazarenolrios@gmail.com>
2022-10-02 21:42:04 -0300

    headers

--- index.js
+++ index.js
@@ -20,6 +20,16 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+app.use((req,res,next) => {
+  res.header('Content-Type', 'application/json;charset=UTF-8')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
 // app.use(express.static("../client/build"))
 
 app.use("/api", routes);
