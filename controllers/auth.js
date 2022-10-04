2c30d84b46e0233347e6c571d80d9510b3946364
NazarenoRios <nazarenolrios@gmail.com>
2022-10-04 17:57:35 -0300

    localstorage

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,7 +36,7 @@ exports.login = (req, res) => {
         pic: user.pic
       };
       const token = tokens.generateToken(payload);
-      res.cookie("token", token);
+      localStorage("token",token)
       res.status(201).send(token);
       console.log(user)
     });
